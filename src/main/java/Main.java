51faa6e24fd715fea2afe27242d88f816a0e4bbb
Elizaveta <ekrivonosva@gmail.com>
2023-12-24 16:42:36 +0300

    method 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        withIn(2,10);
+        withIn(2, 10);
         numberCheck(-10);
+        numCheck(12);
 
     }
-    public static void withIn(int a,int b){
+
+    public static void withIn(int a, int b) {
         int sum = a + b;
-        if(sum >= 10 && sum <= 20){
+        if (sum >= 10 && sum <= 20) {
             System.out.println("True");
-        }
-        else {
+        } else {
             System.out.println("False");
         }
 
     }
+
     public static void numberCheck(int a) {
-        if (a >= 0){
+        if (a >= 0) {
             System.out.println("Положительное число");
-        }
-        else {
+        } else {
             System.out.println("Отрицательное число");
+        }
+    }
+
+    public static void numCheck(int a) {
+        if (a >= 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
         }
     }
 }
