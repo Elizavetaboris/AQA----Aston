338d7983aa84f2bf3e59a62a1caedecd801eae0e
Elizaveta <ekrivonosva@gmail.com>
2023-12-24 18:58:59 +0300

    method 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,12 @@ public class Main {
         withIn(2, 10);
         numberCheck(-10);
         numCheck(12);
-        numStr("Liza",2);
+        numStr("Liza", 2);
         leyeCheck(2024);
+        int[] ar = {1,0,1,0,1,1,0,1,0,1,0,1,};
+        zerArr(ar);
 
     }
-
     public static void withIn(int a, int b) {
         int sum = a + b;
         if (sum >= 10 && sum <= 20) {
@@ -15,9 +16,7 @@ public class Main {
         } else {
             System.out.println("False");
         }
-
     }
-
     public static void numberCheck(int a) {
         if (a >= 0) {
             System.out.println("Положительное число");
@@ -25,7 +24,6 @@ public class Main {
             System.out.println("Отрицательное число");
         }
     }
-
     public static void numCheck(int a) {
         if (a >= 0) {
             System.out.println("True");
@@ -33,16 +31,34 @@ public class Main {
             System.out.println("False");
         }
     }
-    public static void numStr(String a,int b){
-        for(int i=0;i<b;i++){
+    public static void numStr(String a, int b) {
+        for (int i = 0; i < b; i++) {
             System.out.println(a);
         }
     }
-    public static void leyeCheck(int year){
-        if(year % 4 == 0 && year % 100 != 0 || year % 400==0){
+    public static void leyeCheck(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("True");
         } else {
             System.out.println("False");
         }
     }
+    public static void zerArr(int[]ar){
+        {
+            for (int i = 0; i < ar.length; i++) {
+                if (ar[i] == 1) {
+                    ar[i] = 0;
+                } else if (ar[i] == 0) {
+                    ar[i] = 1;
+                }
+            }
+            for (int number : ar) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+
+
 }
+
+
