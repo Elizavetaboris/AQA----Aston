c2c62ddd10d19ad1458235409387dd384c5d9972
Elizaveta <ekrivonosva@gmail.com>
2023-12-24 14:42:45 +0300

    method 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        WithIn(2,10);
+
+    }
+    public static void WithIn(int a,int b){
+        int sum = a + b;
+        if(sum >= 10 && sum <= 20){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
 
     }
 }
