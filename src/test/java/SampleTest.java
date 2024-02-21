2b9979b1bb905dd1a4a1d1dffc68672bd8f5df0e
Elizaveta <ekrivonosva@gmail.com>
2024-02-21 16:23:47 +0300

    lesson_16_test_changes

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,19 +1,66 @@
-import io.restassured.RestAssured;
 import org.testng.annotations.Test;
-
-import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class SampleTest {
-
     @Test
     public void testGetRequest() {
-        // Отправляем GET-запрос и проверяем код ответа и тело ответа
-        RestAssured
+        given()
+                .param("foo", "bar")
                 .when()
                 .get("https://postman-echo.com/get")
                 .then()
-                .statusCode(200) // Проверка кода ответа
-                .body("args.foo", equalTo("bar")); // Проверка тела ответа
+                .statusCode(200)
+                .body("args.foo", equalTo("bar"));
     }
-}
+    @Test
+    public void testPostRequestWithRawText() {
+        String requestBody = "This is a sample raw text for POST request testing";
+
+        given()
+                .contentType("text/plain")
+                .body(requestBody)
+                .when()
+                .post("https://postman-echo.com/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(requestBody));
+    }
+
+    @Test
+    public void testPutRequest() {
+        String requestBody = "This is a sample request body for testing PUT request";
+
+        given()
+                .contentType("text/plain")
+                .body(requestBody)
+                .when()
+                .put("https://postman-echo.com/put")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(requestBody));
+    }
+
+        @Test
+        public void testPatchMethod() {
+            String requestBody = "{\"name\": \"New Name\", \"age\": 30}";
+
+            given()
+                    .header("Content-Type", "application/json")
+                    .body(requestBody)
+                    .when()
+                    .patch("https://postman-echo.com/patch")
+                    .then()
+                    .assertThat()
+                    .statusCode(200)
+                    .body("json.name", equalTo("New Name"))
+                    .body("json.age", equalTo(30));
+        }
+    }
+
+
+
+
+
+
 
